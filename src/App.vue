fe0bc16d96ea9dba54024fa93037420820a97dfc
Alexander Popov <55953131+flamaster00@users.noreply.github.com>
2022-09-07 20:47:06 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue";
+import TestComponent from "@/components/TestComponent";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const text = ref('Hello')
+const count = ref(0)
+const color = ref('red')
+
+function increment() {
+  count.value++
 }
+function update(e) {
+  text.value = e.target.value
+}
+
 </script>
+
+<template>
+  <input type="text" @input="update">
+  <h2 :style="{color}">{{text}}</h2>
+  <h2>{{count}}</h2>
+  <button @click="increment">Click Me!</button>
+
+  <h2>Below is test component!</h2>
+  <TestComponent/>
+</template>
 
 <style>
 #app {
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import {ref} from "vue";
+
+const header = ref('header from TEST component')
+
+</script>
+
+<template>
+<h2>{{header}}</h2>
+</template>
+
+<style>
+
+</style>
