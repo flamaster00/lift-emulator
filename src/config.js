c992a9fe1b1d252e857225e90710f85694e892bd
Alexander Popov <55953131+flamaster00@users.noreply.github.com>
2022-09-12 04:26:28 +0300

    added app settings component, created config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const maxLiftCount = 5
+const minLiftCount = 1
+const maxFloorCount = 10
+const minFloorCount = 5
+
+module.exports = {
+    maxLiftCount, minLiftCount, maxFloorCount, minFloorCount
+}
